ab96abc646201488e3db84c71f4b8862a6929f1e
Adam Edwards <adam@pokelondon.com>
2013-11-01 17:21:49 +0000

    remove get album art function

--- src/pokeradio/public/js/collections/spotify-tracks.js
+++ src/pokeradio/public/js/collections/spotify-tracks.js
@@ -8,16 +8,12 @@ define([
 			url: urls.track,
 			model: Track,
 			initialize: function(){
-				this.on('sync', this.getAlbumArt);
+
 			},
 			parse: function(response){
 				return response.tracks;
 			},
 			getAlbumArt: function(models, resp){
-				href = models.pluck('href');
-				console.log(href);
-				href = href.replace(/spotify/g,"spotify-WW");
-				window.socket.emit('get_album_art', href);
 
 			}
 
