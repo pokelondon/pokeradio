69964ff6a8b1669d4afbaf394f18331067ea4ba4
Adam Edwards <adam@pokelondon.com>
2013-11-01 16:40:16 +0000

    add brukva to setup

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ install_dependencies = [
     'django-debug-toolbar == 0.9.4',
     'tornado == 3.1',
     'simplejson == 3.3.1',
+    'brukva == 0.0.1',
 
 ]
 
@@ -65,7 +66,7 @@ setup(
     install_requires=install_dependencies,
     include_package_data=True,
     dependency_links=[
-        'git://github.com/evilkost/brukva.git#egg=brukva'
+        'git://github.com/evilkost/brukva.git#egg=brukva',
     ],
     extras_require={
         'develop': development_dependencies,
