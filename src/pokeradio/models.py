f29e4dcac41addcd6ec778abb9326878c4c464e1
Eric George <eric@pokelondon.com>
2014-01-08 12:39:52 +0000

    Removed album from models

--- src/pokeradio/models.py
+++ src/pokeradio/models.py
@@ -1,31 +1,25 @@
-from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save, post_delete, pre_save
-from django.dispatch import receiver
-from django.conf import settings
 from calendar import timegm
 import simplejson as json
 import redis
 
-class CommonProperties(models.Model):
-    name = models.CharField(max_length = 255)
-    artist = models.CharField(max_length = 255)
-    href = models.CharField(max_length = 255)
-    timestamp = models.DateTimeField(auto_now_add = True)
+from django.db import models
+from django.db.models.signals import post_save, post_delete, pre_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.conf import settings
 
-    class Meta:
-        abstract = True
 
-class Track(CommonProperties):
-
-    played = models.BooleanField(default = False)
+class Track(models.Model):
+    """ A track in a playlist
+    """
+    name = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    href = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    played = models.BooleanField(default=False)
     user = models.ForeignKey(User)
     length = models.FloatField(null=True)
-    album_href = models.CharField(max_length = 255)
-
-    """TODO:
-    Add validation that prevents user from chossing the same song multiple times in the a timeframe 
-    """
+    album_href = models.CharField(max_length=255)
 
     class Meta:
         ordering = ['timestamp']
@@ -55,16 +49,9 @@ def track_saved(sender, instance, **kwargs):
     r = redis.Redis()
     r.publish('playlist', json.dumps(instance.to_dict()))
 
+
 @receiver(post_delete, sender=Track)
 def track_deleted(sender, **kwargs):
     print 'Deleted!!'
     r = redis.Redis()
     r.publish('playlist_changed', 'DELETED')
-
-class Album(CommonProperties):
-    isrc = models.CharField(max_length = 12, unique = True)
-
-
-
-
-
