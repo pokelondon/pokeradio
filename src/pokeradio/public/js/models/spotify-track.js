6ae28d023a9de95be0d4a62d36c51dac2204636f
Eric George <eric@pokelondon.com>
2014-01-09 18:49:19 +0000

    Checking when queuing whether a track is already in the playlist and bolloking accordinly. Also adding a style for new queued tracks in new searches

--- src/pokeradio/public/js/models/spotify-track.js
+++ src/pokeradio/public/js/models/spotify-track.js
@@ -1,14 +1,31 @@
 define(['jquery',
         'backbone',
         'underscore',
+        'collections/mopidy-playlist'
         ],
-        function($,Backbone,_){
+        function($, Backbone, _, mopidyPlaylist){
             var Track = Backbone.Model.extend({
                 idAttribute: "href",
                 defaults: {
-                    selected: false
+                    selected: false,
+                    inQueue: false
                 },
+
+                initialize: function() {
+                    // Initial value for inqueue to dim out stuff when
+                    // a new search is made
+                    this.set('inQueue', this.checkInPlaylist());
+                },
+
+                /**
+                 * Send a socket message to the server to get
+                 * this track queued
+                 */
                 queue: function() {
+                    if(this.checkInPlaylist()) {
+                        alert(this.get('name') + ' is already queued');
+                        return;
+                    }
                     var track_payload = {
                         'name': this.attributes.name,
                         'href': this.get('href'),
@@ -21,6 +38,15 @@ define(['jquery',
                     socket.emit('add_track', JSON.stringify(track_payload));
                     this.set('selected', !this.get('selected'));
                     // TODO Send socket message to remove from queue if re-clicked
+                },
+
+                /**
+                 * Using the required singleton instance of the mopidy playlist collection
+                 * check whether a track with this spotify URI is already queued.
+                 */
+                checkInPlaylist: function() {
+                    var res = mopidyPlaylist.findWhere({href: this.get('href')});
+                    return !!res;
                 }
         });
         return Track;
