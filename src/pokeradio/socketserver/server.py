f551be8ea63538eba4b61190b8defb3a7cd366da
Eric George <eric@pokelondon.com>
2014-01-10 13:22:23 +0000

    Just moving stuff around i think

--- src/pokeradio/socketserver/server.py
+++ src/pokeradio/socketserver/server.py
@@ -1,17 +1,22 @@
+import logging
 import tornado
-from tornadio2 import SocketConnection
-import simplejson as json
 import brukva
+import simplejson as json
+from tornadio2 import SocketConnection
 from tornadio2 import event, router, server
 from helper import TrackList
-import logging
+
 from django.conf import settings
+
+
 logger = logging.getLogger()
 
 
 class PlayerConnection(SocketConnection):
-    """ Mopidy Endpoint """
-    
+    """ Socket connection for Mopidy client to connect to
+    Bridged to browser connections via Redis PubSub
+    """
+
     def __init__(self, *args, **kwargs):
         super(PlayerConnection, self).__init__(*args, **kwargs)
         self.client = brukva.Client(host='localhost', port=6379, selected_db=0)
@@ -20,27 +25,24 @@ class PlayerConnection(SocketConnection):
 
     def on_open(self, request):
         print 'mopidy connected'
-    
+
     @event('request_track')
     def on_request_track(self, message=''):
-        track_payload = self.tracklist.get_mopidy_track() 
+        track_payload = self.tracklist.get_mopidy_track()
         self.emit('mopidy_play_track', track_payload)
 
     @event('track_playback_ended')
     def on_track_playback_ended(self, href):
        self.tracklist.set_played(href)
        self.on_request_track()
-    
+
     @event('track_playback_started')
     def on_track_playlist_started(self, data):
-        print 'started playing'
-       
+        print 'started playing', data
+
     @event('player_update')
     def on_player_update(self, data):
         self.client.publish('player_update', data)
-    
-    
-
 
 
 class AppConnection(SocketConnection):
@@ -58,47 +60,42 @@ class AppConnection(SocketConnection):
     def on_open(self, request):
         print 'app connected'
         self.client.listen(self.on_redis_message)
-    
 
     def on_redis_message(self, data):
         if data.channel == 'player_update':
             self.emit('playlist:progress', data.body)
         else:
             self.emit('playlist:update', data.body)
-        
-    
+
     @event('add_track')
     def do_add_track(self, new_track):
         user_id = self.session.info.get_cookie('poke_radio_user_id').value
         new_track = json.loads(new_track)
         self.tracklist.add(user_id, new_track)
-    
+
     def on_close(self):
         self.client.unsubscribe('playlist')
-    
+
     @event('playlist')
     def playlist(self, command):
         print command
         if command == 'fetch':
-            self.emit('playlist:fetch',self.tracklist.get_playlist())
+            self.emit('playlist:fetch', self.tracklist.get_playlist())
 
 
 
 class RouterConnection(SocketConnection):
     __endpoints__ = {'/player': PlayerConnection,
-                    '/app':AppConnection}
+                     '/app':AppConnection}
 
     def on_open(self, request):
         pass
 
 
 
-Router = router.TornadioRouter(RouterConnection,
-                                         {'websockets_check': True})
+Router = router.TornadioRouter(RouterConnection, {'websockets_check': True})
 
-app = tornado.web.Application(
-    Router.urls,
-    socket_io_port=settings.SOCKET_PORT)
+app = tornado.web.Application(Router.urls, socket_io_port=settings.SOCKET_PORT)
 
 
 if __name__ == "__main__":
