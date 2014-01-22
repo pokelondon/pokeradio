5090b1491ca4139eaddf4273ffa58fda7b068247
Eric George <eric@pokelondon.com>
2014-01-22 12:24:16 +0000

    Adding tooltip from BS

--- src/pokeradio/public/js/main.js
+++ src/pokeradio/public/js/main.js
@@ -1,31 +1,36 @@
 require.config({
-	paths: {
-		jquery: '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
-		backbone: 'lib/backbone/backbone.min',
-		underscore: 'lib/underscore/underscore',
-		iobind: 'lib/backbone/backbone.iobind.min',
-		iosync: 'lib/backbone/backbone.iosync.min'
-	},
-	shim:{
-		'backbone': {
-			deps: ['underscore','jquery'],
-			exports: 'Backbone'
-		},
-		'underscore': {
-			deps: ['jquery'],
-			exports: '_'
-		},
-		'iosync': {
-			deps: ['jquery', 'underscore', 'backbone'],
-			exports: 'iosync'
-		},
-		'iobind': {
-			deps: ['jquery', 'underscore', 'backbone', 'iosync'],
-			exports: 'iobind'
-		}
-	}
+    paths: {
+        jquery: '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
+        backbone: 'lib/backbone/backbone.min',
+        underscore: 'lib/underscore/underscore',
+        iobind: 'lib/backbone/backbone.iobind.min',
+        iosync: 'lib/backbone/backbone.iosync.min',
+        bootstrap: 'lib/bootstrap/bootstrap'
+    },
+    shim:{
+        'backbone': {
+            deps: ['underscore','jquery'],
+            exports: 'Backbone'
+        },
+        'underscore': {
+            deps: ['jquery'],
+            exports: '_'
+        },
+        'bootstrap': {
+            deps: ['jquery'],
+            exports: '$'
+        },
+        'iosync': {
+            deps: ['jquery', 'underscore', 'backbone'],
+            exports: 'iosync'
+        },
+        'iobind': {
+            deps: ['jquery', 'underscore', 'backbone', 'iosync'],
+            exports: 'iobind'
+        }
+    }
 });
 
 define(['app'],function(app){
-	app.init();
-	});
+    app.init();
+    });
