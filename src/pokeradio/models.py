bada37f6c54a6f8d99708f618d4eedb7def9dcca
Eric George <eric@pokelondon.com>
2014-01-24 11:51:42 +0000

    Sending the playlist updated event when a track finishes playing as well as when they are added. When the event was renamed to track_add it stopped applying but the other type of playlist event was overlooked

--- src/pokeradio/models.py
+++ src/pokeradio/models.py
@@ -73,7 +73,11 @@ def track_saved(sender, instance, created, **kwargs):
         # Deduct a credit for this play
         c = Credit.objects.create(user=instance.user, action='TRACK_ADD',
                 track_name=str(instance)[:100])
+
         r.publish('pr:track_add', json.dumps(instance.to_dict()))
+    else:
+        # Updating a track record, must be marking it as played
+        r.publish('pr:track_played', json.dumps(instance.to_dict()))
 
 
 @receiver(post_delete, sender=Track)
