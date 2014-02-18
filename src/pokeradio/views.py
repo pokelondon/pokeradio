fb425312d67a39bc503361bbfb82ecbf3586d508
Eric George <eric@pokelondon.com>
2014-02-18 18:37:41 +0000

    Initial Leaderboard in the header

--- src/pokeradio/views.py
+++ src/pokeradio/views.py
@@ -1,15 +1,43 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.views.generic import RedirectView
+from django.contrib.auth.models import User
+from django.views.generic import RedirectView, TemplateView
+from django.views.generic.base import ContextMixin
+
+from pokeradio.scoring.models import Point
 
 
-@login_required
-def home(request):
-    return render(request, 'home/index.html', {})
+class HomeView(TemplateView, ContextMixin):
+    template_name = 'home/index.html'
+
+    def get_leaderboard(self):
+        now = datetime.now()
+        a_week = timedelta(weeks=4)
+        this_week = [now - a_week, now]
+
+        qs = User.objects.all()
+        object_list = []
+
+        for i in qs:
+            points = i.point_set.filter(created__range=this_week)
+            likes = points.filter(action=Point.TRACK_LIKED).count()
+            dislikes = points.filter(action=Point.TRACK_DISLIKED).count()
+            if likes < 1 or likes - dislikes < 1:
+                continue
+            object_list.append({'user': i, 'likes': likes,
+                                'dislikes': dislikes})
+        items = sorted(object_list, key=lambda i: i['likes'])
+        items.reverse()
+        return items[:5]
+
+    def get_context_data(self, **kwargs):
+        c = super(HomeView, self).get_context_data(**kwargs)
+        c['leaderboard'] = self.get_leaderboard()
+        return c
 
 
 class WeekArchiveRedirect(RedirectView):
@@ -25,3 +53,5 @@ class WeekArchiveRedirect(RedirectView):
         if 'who' in kwargs:
             url_params['who'] = kwargs.get('who')
         return reverse(self.pattern, kwargs=url_params)
+
+home = login_required(HomeView.as_view())
