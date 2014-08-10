73624b52c69cec7a0c5ec363746ee209b2c0483f
Eric George <eric@pokelondon.com>
2014-08-10 15:14:37 +0100

    Stacking messages

--- src/pokeradio/public/js/views/messaging/controller.js
+++ src/pokeradio/public/js/views/messaging/controller.js
@@ -16,6 +16,33 @@ define(
                 this.collection.on('add', this.displayMessage, this);
 
                 // TODO: listen for socket events to show messages
+                this.createMessage({
+                    text: "An message",
+                    modal: true,
+                    type: 'bad'
+                });
+                setTimeout(function() {
+                    self.createMessage({
+                        text: "An uva message this one is a bit longer to see what happens when its a bit longer than what the other ones are",
+                        modal: true,
+                        type: 'good'
+                    });
+                }, 400);
+
+                var self = this;
+                setTimeout(function() {
+                    self.createMessage({
+                        text: "An uva nuva message",
+                        modal: true,
+                        type: 'bad'
+                    });
+                }, 600);
+                setTimeout(function() {
+                    self.createMessage({
+                        text: "An uva aaanuvanuva message",
+                        modal: true
+                    });
+                }, 1600);
             },
 
             createMessage: function(options) {
