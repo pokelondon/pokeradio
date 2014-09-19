e071a755089606132ae97a4425df0fbd4d8f0436
Eric George <eric@pokelondon.com>
2014-09-19 14:38:04 +0100

    Getting album art from spotify. much fewer requests, and quicker api. Still caching by album id in nginx, so existing assets should be ok

--- src/pokeradio/albumart/views.py
+++ src/pokeradio/albumart/views.py
@@ -1,29 +1,39 @@
+import spotipy
+
 from django.conf import settings
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 
-from pokeradio.albumart.tasks import get_image_url
 from pokeradio.tasks import create_image
 
 from pokeradio.history.models import ArchiveTrack
 
+sp = spotipy.Spotify()
 
-def image(request, code, *args, **kwargs):
+
+def image(request, album_id, *args, **kwargs):
     """
     if NGINX fails to find a jpg it is then routed to django
-    to fetch the image from musicbrainz
+    to fetch the image from spotify
     """
+    full_album_id = 'spotify:album:{0}'.format(album_id)
+
+    res = sp.album(full_album_id)
+    try:
+        url = res['images'][-1]['url']
+    except KeyError:
+        return HttpResponseRedirect(settings.ALBUM_ART_FALLBACK)
 
     response = HttpResponse(mimetype="image/jpg")
+
     try:
-        image_url =  get_image_url(code)
-        image, fsize  = create_image(image_url, code,
+        image, fsize  = create_image(url, album_id,
                                      settings.ALBUM_ART_BASE_DIR,
                                      settings.THUMBNAIL_SIZE)
     except Http404:
         # Mark tracks as not having an image (for speed)
-        res = ArchiveTrack.objects.by_album_code(code=code).update(
+        res = ArchiveTrack.objects.get(album_href=full_album_id).update(
                 no_artwork=True)
-        print 'No artwork for', res, 'items', code
+        print 'No artwork for', res, 'items', full_album_id
         return HttpResponseRedirect(settings.ALBUM_ART_FALLBACK)
     else:
         image.save(response, 'JPEG')
