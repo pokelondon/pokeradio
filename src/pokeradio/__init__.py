87481f949ecca5b30c4236840601b12f432561bc
Adam Edwards <adam@pokelondon.com>
2014-09-19 15:20:20 +0100

    catch import error for celery

--- src/pokeradio/__init__.py
+++ src/pokeradio/__init__.py
@@ -1,7 +1,13 @@
 from __future__ import absolute_import
+import sys
+
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
-from .celery import app as celery_app
+try:
+    from .celery import app as celery_app
+except ImportError:
+    sys.stdout.write('Celery not found')
+
 
 __VERSION__ = '0.1dev'
