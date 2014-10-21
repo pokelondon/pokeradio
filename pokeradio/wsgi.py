0222efdacaf4a1b6216f9ac2430d4de40a7444ee
Eric George <eric@pokelondon.com>
2014-10-21 11:30:35 +0100

    Adjusting fallback config specification, just in case

--- pokeradio/wsgi.py
+++ pokeradio/wsgi.py
@@ -6,7 +6,7 @@
 import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                      "pokeradio.config.live.settings")
+                      "pokeradio.config.settings")
 
 # This application object is used by the development server
 # as well as any WSGI server configured to use this file.
