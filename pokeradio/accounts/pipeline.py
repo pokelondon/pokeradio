947c391b38bfbc5f4c0894e662298f12e6f44a0c
Eric George <eric@pokelondon.com>
2014-10-21 12:02:29 +0100

    Optional profile image save

--- pokeradio/accounts/pipeline.py
+++ pokeradio/accounts/pipeline.py
@@ -2,25 +2,17 @@ from django.conf import settings
 from pokeradio.tasks import create_image
 
 def get_profile_pic(strategy, details, response, user=None, is_new=False, *args, **kwargs):
-	
-	""" TODO: Get new image every 24 hours """
-	def save_image():	
-		image_url = response.get('picture','default')
+    """ TODO: Get new image every 24 hours """
+    def save_image():       
+        image_url = response.get('picture','default')
 
-		if image_url != 'default':
-			user_id = str(user.id)
-			create_image(image_url, user_id, settings.USER_PROFILE_PICTURES_BASE_DIR, settings.PROFILE_PICTURE_SIZE)
+        if image_url != 'default':
+            user_id = str(user.id)
+            try:
+                create_image(image_url, user_id,
+                            settings.USER_PROFILE_PICTURES_BASE_DIR,
+                            settings.PROFILE_PICTURE_SIZE)
+            except IOError:
+                pass
 
-	save_image()
-		
-
-
-
-		
-
-	
-
-
-
-
-
+    save_image()
